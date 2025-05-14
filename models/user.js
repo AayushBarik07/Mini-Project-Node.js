8ea28f3e9a727b96c4d614d484f2b4febee33bfa
Aayush <superbsup45@gmail.com>
2025-05-14 10:17:29 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/mini-project');
+
+const userSchema = mongoose.Schema ({
+  username: String, 
+  password: String,
+  name: String,
+  email: String,
+  age: Number,
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post"
+    }
+  ]
+});
+
+module.exports = mongoose.model("user", userSchema);
